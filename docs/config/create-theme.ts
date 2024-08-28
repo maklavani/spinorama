9c1784d609f04701febdf8ef9a775ec079bba8a5
Hossein Maklavani <hos.mohamadi@gmail.com>
2024-08-28 17:08:39 +0330

    0.5.10

---
+++ docs/config/create-theme.ts
@@ -0,0 +1,46 @@
+import { createTheme } from '@mui/material/styles'
+
+// Types
+import type { Direction } from '@mui/material/styles'
+
+// Fonts
+import RubikFont from '@/styles/fonts/rubik'
+import RobotoFont from '@/styles/fonts/roboto'
+
+const CreateThemeConfig = (dir: Direction) =>
+	createTheme({
+		direction: dir,
+		cssVariables: true,
+		colorSchemes: {
+			light: {
+				palette: {
+					mode: 'light',
+					primary: { light: '#b199f4', main: '#7d54ed', dark: '#511ae7', contrastText: '#fff' },
+					secondary: { light: '#e8b6e1', main: '#ba39a7', dark: '#912c82', contrastText: '#fff' }
+				}
+			},
+			dark: {
+				palette: {
+					mode: 'dark',
+					primary: { light: '#b199f4', main: '#7d54ed', dark: '#511ae7', contrastText: '#fff' },
+					secondary: { light: '#e8b6e1', main: '#ba39a7', dark: '#912c82', contrastText: '#fff' },
+					background: { default: '#0f132e', paper: '#252f72' }
+				}
+			}
+		},
+		shape: { borderRadius: 8 },
+		typography: {
+			fontFamily: `${RubikFont.style.fontFamily}, ${RobotoFont.style.fontFamily}`,
+			fontSize: 14
+		},
+		components: {
+			MuiCssBaseline: {
+				styleOverrides: {
+					body: { scrollBehavior: 'smooth' },
+					a: { textDecoration: 'none' }
+				}
+			}
+		}
+	})
+
+export default CreateThemeConfig
